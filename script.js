cab560a5a18f54a8f5901505289540d06549361c
MandeepMann <mandeep_mann30@hotmail.com>
2022-11-09 17:58:37 -0500

    displayed games and details on page

--- script.js
+++ script.js
@@ -19,3 +19,34 @@ steamApp.storeSearch = () => {
             steamApp.displayGame(data.items);
         })
 }
+
+steamApp.displayGame = (gamesList) => {
+    gamesList.forEach((game) => {
+        const newLiElement = document.createElement('li');
+        newLiElement.innerHTML = `
+        <img src=${game.tiny_image} alt=${game.name} />
+        <h2>${game.name}</h2>
+        `;
+
+        const gamePriceEl = document.createElement('p');
+        const getPrice = (gamePrice) => {
+            if (gamePrice.price) {
+                gamePriceEl.innerHTML = `<a href="https://store.steampowered.com/app/${game.id}">${game.price.final}</a>`;
+            } else {
+                gamePriceEl.innerHTML = `<a href="https://store.steampowered.com/app/${game.id}">Pricing Info Here</a>`;
+            }
+        }
+        getPrice(game);
+        const gamesUl = document.querySelector('.list');
+        gamesUl.append(newLiElement);
+        newLiElement.append(gamePriceEl);
+        steamApp.gamingPlatforms(gamesList);
+    });
+}
+
+
+steamApp.init = () => {
+    steamApp.storeSearch();
+}
+
+steamApp.init();
